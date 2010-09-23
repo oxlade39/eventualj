558f8ef9d603cf57bb27a7d1097e81cd276d4740
oxlade39 <dan.oxlade@gmail.com>
2010-09-23 20:51:38 +0100

    initial import of maven proj

---
+++ src/main/java/org/doxla/jeventual/App.java
@@ -0,0 +1,13 @@
+package org.doxla.jeventual;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
